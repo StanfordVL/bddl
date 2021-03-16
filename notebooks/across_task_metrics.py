9aade5fefc43e3abd49c4831b59b9a923fc41bfb
Sanjana Srivastava <sanjana2@stanford.edu>
2021-03-16 15:18:40 -0500

    corrs done

--- notebooks/across_task_metrics.py
+++ notebooks/across_task_metrics.py
@@ -57,7 +57,7 @@ def convert_name(name):
 # -
 
 
-# # Distance
+# # Distance, Work
 
 # +
 records = []
@@ -67,7 +67,10 @@ columns = [
     "scene_id",
     "left_total_dist",
     "right_total_dist",
-    "body_total_dist"
+    "body_total_dist",
+    "left_total_work",
+    "right_total_work",
+    "body_total_work"
 ]
 
 clip = .2
@@ -95,10 +98,29 @@ for demo in raw_data:
     record.append(np.sum(right_delta_position))
     record.append(np.sum(body_delta_position))
     
+    # work (only translational forces)
+    left_force = demo['vr']['vr_device_data']['left_controller'][2:, 17:20]
+    right_force = demo['vr']['vr_device_data']['right_controller'][2:, 17:20]
+    body_force = demo['vr']['vr_device_data']['vr_position_data'][2:, 6:9]
+    
+    left_force = np.linalg.norm(left_force, axis=1)
+    right_force = np.linalg.norm(right_force, axis=1)
+    body_force = np.linalg.norm(body_force, axis=1)
+
+    left_work = left_delta_position * left_force
+    right_work = right_delta_position * right_force
+    body_work = body_delta_position * body_force
+    
+    record.append(np.sum(left_work))
+    record.append(np.sum(right_work))
+    record.append(np.sum(body_work))
+    
     records.append(record)
     
 
 dist_plotting_data = pd.DataFrame.from_records(records, columns=columns)
+dist_plotting_data["task_label"] = dist_plotting_data["task_name"] + "_" + dist_plotting_data["task_instance"].astype(str)
+
 # -
 
 # ### Total distance traveled by task - randomly selected scene
@@ -106,22 +128,21 @@ dist_plotting_data = pd.DataFrame.from_records(records, columns=columns)
 # +
 # randomly select scenes for some (task, task_id)
 choice_fn = lambda obj: obj.loc[np.random.choice(obj.index), :]
-dist_data_randomscenes = dist_plotting_data.groupby(["task_name", "task_instance"], as_index=False).apply(choice_fn)
+dist_data_randomscenes = dist_plotting_data.groupby(["task_label"], as_index=False).apply(choice_fn)
 
 hued_dfs = [
-    dist_data_randomscenes[["task_name", "task_instance", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
-    dist_data_randomscenes[["task_name", "task_instance", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
-    dist_data_randomscenes[["task_name", "task_instance", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
+    dist_data_randomscenes[["task_label", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
+    dist_data_randomscenes[["task_label", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
+    dist_data_randomscenes[["task_label", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
 ]
 hued_dfs[0]["device"] = "left"
 hued_dfs[1]["device"] = "right"
 hued_dfs[2]["device"] = "body"
 
 hued_dist_data_randomscenes = pd.concat(hued_dfs)
-hued_dist_data_randomscenes["task_label"] = hued_dist_data_randomscenes["task_name"] + "_" + hued_dist_data_randomscenes["task_instance"].astype(str)
 # -
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.barplot(
     x="task_label",
     y="total_dist",
@@ -135,22 +156,22 @@ plt.savefig('total_distance_randomscenes.pdf')
 
 # +
 # average distances across different scenes 
-dist_data_avg = dist_plotting_data.groupby(['task_name', 'task_instance'], as_index=False).agg(np.mean)
+dist_data_avg = dist_plotting_data.groupby(['task_label'], as_index=False).agg(np.mean)
 
 hued_dfs = [
-    dist_data_avg[["task_name", "task_instance", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
-    dist_data_avg[["task_name", "task_instance", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
-    dist_data_avg[["task_name", "task_instance", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
+    dist_data_avg[["task_label", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
+    dist_data_avg[["task_label", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
+    dist_data_avg[["task_label", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
 ]
 hued_dfs[0]["device"] = "left"
 hued_dfs[1]["device"] = "right"
 hued_dfs[2]["device"] = "body"
 
 hued_dist_data_avg = pd.concat(hued_dfs)
-hued_dist_data_avg["task_label"] = hued_dist_data_avg["task_name"] + '_' + hued_dist_data_avg["task_instance"].astype(str)
+
 # -
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.barplot(
     x="task_label",
     y="total_dist",
@@ -158,73 +179,82 @@ ax = sns.barplot(
     data=hued_dist_data_avg
 )
 plt.xticks(rotation=90)
-plt.savefig('total_distance_avgacrossscenes.pdf')
+plt.savefig('total_distance_avg.pdf')
 
 # ### Total distance traveled by task - whisker plot across scenes 
 
 # +
 hued_dfs = [
-    dist_plotting_data[["task_name", "task_instance", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
-    dist_plotting_data[["task_name", "task_instance", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
-    dist_plotting_data[["task_name", "task_instance", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
+    dist_plotting_data[["task_label", "left_total_dist"]].rename(columns={"left_total_dist": "total_dist"}),
+    dist_plotting_data[["task_label", "right_total_dist"]].rename(columns={"right_total_dist": "total_dist"}),
+    dist_plotting_data[["task_label", "body_total_dist"]].rename(columns={"body_total_dist": "total_dist"})
 ]
 hued_dfs[0]["device"] = "left"
 hued_dfs[1]["device"] = "right"
 hued_dfs[2]["device"] = "body"
 
 hued_plotting_data = pd.concat(hued_dfs)
-hued_plotting_data["task_label"] = hued_plotting_data["task_name"] + '_' + hued_plotting_data["task_instance"].astype(str)
 # -
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 # ax = sns.stripplot(x="task_label", y="total_dist", data=hued_plotting_data, jitter=0.05)
 ax = sns.violinplot(x="task_label", y="total_dist", hue="device", data=hued_plotting_data)
 plt.xticks(rotation=90)
 plt.savefig("total_distance_range.pdf")
 
 
-# # Work
+# ### Total work by task - avg across scenes
 
 # +
-records = []
-columns = [
-    "task_name",
-    "task_instance",
-    "scene_id",
+# average distances across different scenes 
+dist_data_avg = dist_plotting_data.groupby(['task_label'], as_index=False).agg(np.mean)
+
+hued_dfs = [
+    dist_data_avg[["task_label", "left_total_work"]].rename(columns={"left_total_work": "total_work"}),
+    dist_data_avg[["task_label", "right_total_work"]].rename(columns={"right_total_work": "total_work"}),
+    dist_data_avg[["task_label", "body_total_work"]].rename(columns={"body_total_work": "total_work"})
 ]
+hued_dfs[0]["device"] = "left"
+hued_dfs[1]["device"] = "right"
+hued_dfs[2]["device"] = "body"
 
-for demo in raw_data: 
-    record = []
-    record.append(convert_name(demo.attrs["/metadata/task_name"]))
-    record.append(demo.attrs["/metadata/task_instance"])
-    record.append(demo.attrs["/metadata/scene_id"][:-4])
-    
-    # distance 
-    left_position = demo["vr"]["vr_device_data"]["left_controller"][:, 1:4]
-    right_position = demo["vr"]["vr_device_data"]["right_controller"][:, 1:4]
-    body_position = demo["vr"]["vr_device_data"]["vr_position_data"][:, 0:3]
-    
-    left_delta_position = np.linalg.norm(left_position[1:-1] - left_position[0:-2], axis=1)
-    right_delta_position = np.linalg.norm(right_position[1:-1] - right_position[0:-2], axis=1)
-    body_delta_position = np.linalg.norm(body_position[1:-1] - body_position[0:-2], axis=1)
-
-    left_delta_position = np.clip(-0.2, 0.2, left_delta_position)
-    right_delta_position = np.clip(-0.2, 0.2, right_delta_position)
-    body_delta_position = np.clip(-0.2, 0.2, body_delta_position)
-    
-    # force
-    left_force = demo['vr']['vr_device_data']['left_controller'][1:, 17:23]
-    right_force = demo['vr']['vr_device_data']['right_controller'][1:, 17:23]
-    body_force = demo['vr']['vr_device_data']['vr_position_data'][1:, 6:12]
-    
-    # total work 
-    
-    
-    records.append(record)
-    
-
-dist_plotting_data = pd.DataFrame.from_records(records, columns=columns)
+hued_dist_data_avg = pd.concat(hued_dfs)
 # -
+
+fig = plt.figure(figsize=(10, 6))
+ax = sns.barplot(
+    x="task_label",
+    y="total_work",
+    hue="device",
+    data=hued_dist_data_avg
+)
+plt.xticks(rotation=90)
+plt.savefig('total_work_avg.pdf')
+
+# ### Total work by task - range across scenes
+
+# +
+hued_dfs = [
+    dist_plotting_data[["task_label", "left_total_work"]].rename(columns={"left_total_work": "total_work"}),
+    dist_plotting_data[["task_label", "right_total_work"]].rename(columns={"right_total_work": "total_work"}),
+    dist_plotting_data[["task_label", "body_total_work"]].rename(columns={"body_total_work": "total_work"})
+]
+hued_dfs[0]["device"] = "left"
+hued_dfs[1]["device"] = "right"
+hued_dfs[2]["device"] = "body"
+
+hued_plotting_data = pd.concat(hued_dfs)
+# -
+
+fig = plt.figure(figsize=(10, 6))
+# ax = sns.stripplot(x="task_label", y="total_dist", data=hued_plotting_data, jitter=0.05)
+ax = sns.violinplot(
+    x="task_label", 
+    y="total_work", 
+    hue="device", 
+    data=hued_plotting_data)
+plt.xticks(rotation=90)
+plt.savefig("total_work_range.pdf")
 
 # # Time to success 
 
@@ -307,7 +337,7 @@ success_plotting_data_nofailure = success_plotting_data[success_plotting_data["s
 choice_fn = lambda obj: obj.loc[np.random.choice(obj.index), :]
 success_data_randomscenes = success_plotting_data_nofailure.groupby(["task_name", "task_instance"], as_index=False).apply(choice_fn)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.barplot(
     x="task_label",
     y="frames_to_success",
@@ -323,25 +353,25 @@ success_data_avg = success_plotting_data_nofailure.groupby(['task_name', 'task_i
 success_data_avg["task_label"] = success_data_avg["task_name"] + "_" + success_data_avg["task_instance"].astype(str)
 
 # +
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.barplot(
     x="task_label",
     y="frames_to_success",
     data=success_data_avg)
 
 plt.xticks(rotation=90)
-plt.savefig('frames_to_success_avgacrossscenes.pdf')
+plt.savefig('frames_to_success_avg.pdf')
 
 # +
 # average across scenes for some (task, task_id)
 success_plotting_data_nofailure = success_plotting_data[success_plotting_data["success"] == 1]
 
-success_data_avgacrossscenes = success_plotting_data_nofailure.groupby(['task_name', 'task_instance'], as_index=False).agg(np.mean)
+success_data_avgacrossscenes = success_plotting_data_nofailure.groupby(['task_label'], as_index=False).agg(np.mean)
 # -
 
 # ### Total frames to reach success ONLY FOR SUCCESSFUL DEMOS - range
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.violinplot(
     x="task_label",
     y="frames_to_success",
@@ -355,40 +385,139 @@ plt.savefig('frames_to_success_range.pdf')
 # ### Total frames to reach success ONLY FOR SUCCESSFUL DEMOS - average, normalized by num ground goal conditions
 
 # +
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax = sns.barplot(
     x="task_label",
     y="frames_to_success_normalized_ground",
     data=success_data_avg)
 
 plt.xticks(rotation=90)
-plt.savefig('frames_to_success_avgacrossscenes.pdf')
+plt.savefig('frames_to_success_avg_groundnorm.pdf')
 # -
 
-# # Correlation of grasps + time to reach success
+# # Correlation of task duration and efficiency
 
-# ### Total grasps vs. frames to success
+success_dist_plotting_data = pd.merge(dist_plotting_data, success_plotting_data_nofailure, on=["task_label", "scene_id"], how="inner")
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(16, 8))
+# ### Frames to success vs. total work
+
+# +
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9)) = plt.subplots(3, 3, figsize=(30, 24))
+
+# no normalization
 sns.regplot(
-    x="frames_to_success",
-    y="total_grasps",
-    data=success_plotting_data_nofailure,
+    x="left_total_work",
+    y="frames_to_success",
+    data=success_dist_plotting_data,
     ax=ax1
 )
+print(success_dist_plotting_data["left_total_work"].corr(success_dist_plotting_data["frames_to_success"]))
+
 sns.regplot(
-    x="frames_to_success_normalized_goal",
-    y="total_grasps",
-    data=success_plotting_data_nofailure,
+    x="right_total_work",
+    y="frames_to_success",
+    data=success_dist_plotting_data,
     ax=ax2
 )
+print(success_dist_plotting_data["right_total_work"].corr(success_dist_plotting_data["frames_to_success"]))
+
 sns.regplot(
-    x="frames_to_success_normalized_ground",
-    y="total_grasps",
-    data=success_plotting_data_nofailure,
+    x="body_total_work",
+    y="frames_to_success",
+    data=success_dist_plotting_data,
     ax=ax3
 )
+print(success_dist_plotting_data["body_total_work"].corr(success_dist_plotting_data["frames_to_success"]))
 
+# goal cond normalization
+sns.regplot(
+    x="left_total_work",
+    y="frames_to_success_normalized_goal",
+    data=success_dist_plotting_data,
+    ax=ax4
+)
+print(success_dist_plotting_data["left_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_goal"]))
+
+sns.regplot(
+    x="right_total_work",
+    y="frames_to_success_normalized_goal",
+    data=success_dist_plotting_data,
+    ax=ax5
+)
+print(success_dist_plotting_data["right_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_goal"]))
+
+sns.regplot(
+    x="body_total_work",
+    y="frames_to_success_normalized_goal",
+    data=success_dist_plotting_data,
+    ax=ax6
+)
+print(success_dist_plotting_data["body_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_goal"]))
+
+# ground goal normalization
+sns.regplot(
+    x="left_total_work",
+    y="frames_to_success_normalized_ground",
+    data=success_dist_plotting_data,
+    ax=ax7
+)
+print(success_dist_plotting_data["left_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_ground"]))
+
+sns.regplot(
+    x="right_total_work",
+    y="frames_to_success_normalized_ground",
+    data=success_dist_plotting_data,
+    ax=ax8
+)
+print(success_dist_plotting_data["right_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_ground"]))
+
+sns.regplot(
+    x="body_total_work",
+    y="frames_to_success_normalized_ground",
+    data=success_dist_plotting_data,
+    ax=ax9
+)
+print(success_dist_plotting_data["body_total_work"].corr(success_dist_plotting_data["frames_to_success_normalized_ground"]))
+
+plt.savefig("duration_vs_work.pdf")
+
+# -
+
+# # Correlation of grasps + work
+
+# ### Total grasps vs. total work 
+
+# +
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30, 8))
+
+# no normalization
+sns.regplot(
+    x="left_total_work",
+    y="total_grasps",
+    data=success_dist_plotting_data,
+    ax=ax1
+)
+print(success_dist_plotting_data["left_total_work"].corr(success_dist_plotting_data["total_grasps"]))
+
+sns.regplot(
+    x="right_total_work",
+    y="total_grasps",
+    data=success_dist_plotting_data,
+    ax=ax2
+)
+print(success_dist_plotting_data["right_total_work"].corr(success_dist_plotting_data["total_grasps"]))
+
+sns.regplot(
+    x="body_total_work",
+    y="total_grasps",
+    data=success_dist_plotting_data,
+    ax=ax3
+)
+print(success_dist_plotting_data["body_total_work"].corr(success_dist_plotting_data["total_grasps"]))
+
+plt.savefig("grasps_vs_work.pdf")
+
+# -
 
 # # Failure rates
 
@@ -416,7 +545,14 @@ sns.barplot(
 )
 # plt.xticks(rotation=90)
 ax2.set_xticklabels(ax2.get_xticklabels(), rotation=90)
+plt.savefig("failure_rate.pdf")
 
 # -
+
+
+
+
+
+
 
 
