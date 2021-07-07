6f59613225b294ea496445496925565ca5eec99d
sanjanasrivastava <sanjana2@stanford.edu>
2021-07-07 13:19:21 -0500

    minor

--- behavior/backend_abc.py
+++ behavior/backend_abc.py
@@ -6,5 +6,5 @@ from future.utils import with_metaclass
 class BEHAVIORBackend(with_metaclass(ABCMeta)):
     @abstractmethod
     def get_predicate_class(self, predicate_name):
-        """Given predicate_name, return an implementation of behavior.logic_base.AtomicPredicate or subclasses."""
+        """Given predicate_name, return an implementation of behavior.logic_base.AtomicFormula or subclasses."""
         pass
